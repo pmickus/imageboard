b2cf2d8b808a323674e7c796675bd87593467c4b
Paul Mickus <pmickus@gmail.com>
2013-08-13 15:15:12 -0400

    ImageBoard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imageboard::Application.config.secret_key_base = '2d6281631c04cf234b8f48fb19e643727f8eee2cebb6faaaba0107b5adae9e751684137e5989a917c618e41fe8fad645e08a0a056610ade5b21a81b7530ff44b'
