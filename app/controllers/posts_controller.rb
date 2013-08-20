bf5afa0c9c148fb704ecb819b9f73cf0e2ac9f12
Paul Mickus <pmickus@gmail.com>
2013-08-20 03:33:24 -0400

    more basic work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,10 +18,6 @@ class PostsController < ApplicationController
     @post = @thread.posts.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
   # POST /posts
   # POST /posts.json
   def create
@@ -29,36 +25,12 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @thread, notice: 'Post was successfully created.' }
+        format.html { redirect_to thread_posts_url, notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
         format.html { render action: 'new' }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -10,15 +10,12 @@ class ThreadsController < ApplicationController
   # GET /threads/1
   # GET /threads/1.json
   def show
+    redirect_to controller: 'posts', action: 'index', thread_id: @thread.id
   end
 
   # GET /threads/new
   def new
     @thread = Threads.new
-  end
-
-  # GET /threads/1/edit
-  def edit
   end
 
   # POST /threads
@@ -37,30 +34,6 @@ class ThreadsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /threads/1
-  # PATCH/PUT /threads/1.json
-  def update
-    respond_to do |format|
-      if @thread.update(thread_params)
-        format.html { redirect_to @thread, notice: 'Threads was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @thread.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /threads/1
-  # DELETE /threads/1.json
-  def destroy
-    @thread.destroy
-    respond_to do |format|
-      format.html { redirect_to threads_index_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_thread
@@ -69,6 +42,6 @@ class ThreadsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def thread_params
-      params[:thread]
+      params[:thread].permit(:title)
     end
 end
